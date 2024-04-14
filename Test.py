b6b400c28f369b71a638ebde5269f17cae5f6e2b
Naiko02 <Naiko02@mail.ru>
2024-04-14 14:14:59 +0300

    update

--- Test.py
+++ Test.py
@@ -1,47 +1,84 @@
 import time
 import pytest
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import sys
-print(sys.getrecursionlimit())
-
 
 browser = webdriver.Chrome()
-browser.get('https://arnypraht.com')
+browser.get('https://demowebshop.tricentis.com/')
 def FindElem(data):
     print(data)
     return browser.find_element(data[0], data[1])
 
 NavData={
-    'Сумки': [By.LINK_TEXT, 'Сумки'],
-    'Рюкзаки': [By.LINK_TEXT, 'Рюкзаки'],
-    'Аксессуары': [By.LINK_TEXT, 'Аксессуары'],
-    'Одежда': [By.LINK_TEXT, 'Одежда']
+    'BOOKS': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/books"]'],
+    'COMPUTERS': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/computers"]'],
+    'ELECTONICS': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/electronics'],
+    'APPAREL & SHOES': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/apparel-shoes"]'],
+    'DIGITAL DOWNLOADS': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/digital-downloads"]'],
+    'JEWELRY': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/jewelry"]'],
+    'GIFT CARDS': [By.CSS_SELECTOR, 'ul[class="top-menu"] a[href="/gift-cards"]'],
+    'TEST': [By.CSS_SELECTOR, 'div[class="breadcrumb"] strong[class="current-item"]']
 }
+class TestNav():
+    def test_Navigation_1(self):
 
-def test_Navigation_1():
+        click_button = FindElem(NavData['BOOKS'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert(current_page.text == 'BOOKS')
+    def test_Navigation_2(self):
 
-    click_button = FindElem(NavData['Сумки'])
-    print(click_button)
-    click_button.click()
-    time.sleep(2)
+        click_button = FindElem(NavData['COMPUTERS'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'COMPUTERS')
 
-def test_Navigation_2():
-    click_button = FindElem(NavData['Рюкзаки'])
-    print(click_button)
-    click_button.click()
-    time.sleep(2)
+    def test_Navigation_3(self):
 
-def test_Navigation_3():
-    click_button = FindElem(NavData['Аксессуары'])
-    print(click_button)
-    click_button.click()
-    time.sleep(2)
+        click_button = FindElem(NavData['ELECTONICS'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'ELECTRONICS')
 
-def test_Navigation_4():
-    click_button = FindElem(NavData['Одежда'])
-    print(click_button)
-    click_button.click()
-    time.sleep(2)
+    def test_Navigation_4(self):
 
+        click_button = FindElem(NavData['APPAREL & SHOES'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'APPAREL & SHOES')
 
+    def test_Navigation_5(self):
+
+        click_button = FindElem(NavData['DIGITAL DOWNLOADS'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'DIGITAL DOWNLOADS')
+
+    def test_Navigation_6(self):
+
+        click_button = FindElem(NavData['JEWELRY'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'JEWELRY')
+
+    def test_Navigation_7(self):
+
+        click_button = FindElem(NavData['GIFT CARDS'])
+        print(click_button)
+        click_button.click()
+        time.sleep(1)
+        current_page = FindElem(NavData['TEST'])
+        assert (current_page.text == 'GIFT CARDS')
